d41a0e13bb6ca5e12f784b13204045602f5a0a73
LouisNaets <105418482+LouisNaets@users.noreply.github.com>
2024-09-11 13:35:44 +0200

    .

--- A1 Aeroelastic design/Part_1.py
+++ A1 Aeroelastic design/Part_1.py
@@ -13,15 +13,16 @@ Cl_design = (2*m.pi)**2/360 * aoa_design + 0.452
 c_R = (16*m.pi/9) * (1 / (Cl_design * TSR**2 * B)) * 1/r_R
 theta_R = 2/3 * (1/TSR * 1/r_R) - aoa_design # [rad]
 
-# Plot c/r and theta/r as a function of r/R in 2 different plots
+# Plot c/r and theta/r as a function of r/R in subplots
 plt.figure()
+plt.subplot(2, 1, 1)
 plt.plot(r_R, c_R)
 plt.xlabel('r/R [-]')
 plt.ylabel('c/R [-]')
 plt.title('Chord distribution')
 plt.grid()
 
-plt.figure()
+plt.subplot(2, 1, 2)
 plt.plot(r_R, np.rad2deg(theta_R))
 plt.xlabel('r/R [-]')
 plt.ylabel('Twist [deg]')
