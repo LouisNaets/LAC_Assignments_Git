3955935ad3c0bb31f4a79c4da75c7e8fd08fc9a8
LouisNaets <105418482+LouisNaets@users.noreply.github.com>
2024-10-09 15:01:14 +0200

    Re added the A2 make_htc

--- hawc_files/our_design/make_htc_files_A3.py
+++ hawc_files/our_design/make_htc_files_A3.py
@@ -5,14 +5,12 @@ Requires myteampack (which requires lacbox).
 from myteampack import MyHTC
 
 if __name__ == '__main__':
-    ORIG_PATH = './hawc_files/our_design/_master/group7_3B_design_old_st.htc'
+    ORIG_PATH = './hawc_files/our_design/_master/group7_3B_design.htc'
     SAVE_HAWC2S_DIR = './hawc_files/our_design'
 
     # make htc file for tuning controller parameters
     htc = MyHTC(ORIG_PATH)
-    htc.make_hawc2s(SAVE_HAWC2S_DIR,
+    htc.make_hawc2s_ctrltune(save_dir=SAVE_HAWC2S_DIR,
                     rigid=False,
                     append='_controller_tuning',
-                    opt_path='./data/dtu_10mw_rigid.opt',
-                    torque_controller={'natural_frequency': 0.05, 'damping_ratio': 0.7},
-                    pitch_controller={'natural_frequency': 0.06, 'damping_ratio': 0.7})
+                    opt_path='./data/dtu_10mw_rigid.opt')
