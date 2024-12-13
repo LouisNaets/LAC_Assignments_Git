256932bb526a46ed98cdd70d341908bd7d3badc1
tbseverin <38481744+tbseverin@users.noreply.github.com>
2024-12-13 14:11:17 +0100

    Skipped part 2 went straight to part 3: control. The ctrl tune hawc file has beern run (step1) and now need to update the neccessary parameters for correct theoretical calulcations

--- A3 Controller design/Assignment/Theoretical calculations.py
+++ A3 Controller design/Assignment/Theoretical calculations.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 from lacbox.io import load_ctrl_txt
 
 plt.rcParams.update({'axes.labelsize': 14, 'xtick.labelsize': 12, 'ytick.labelsize': 12, 'legend.fontsize': 10, 'axes.titlesize': 15})
-
-theta = [0.69990, 1.33032, 1.87337, 2.34564, 2.78268, 3.18625, 3.55478, 6.41687, 8.55115,10.35828,11.97465,13.46319,14.85867,16.18002,17.44155,18.65245,19.81880,20.94841,22.04174,23.10374]
-
-dQ_dtheta = [-1250.21385,-1294.80527,-1336.27132,-1391.04065,-1441.85613,-1486.51244,-1537.42141,-1817.22625,-2099.38349,-2355.84289,-2573.84846,-2803.55880,-3034.19836,-3248.08553,-3467.85105,-3684.03512,-3906.81836,-4126.44262,-4349.84614,-4582.75853]
+#EDITED: theta and dQ_dtheta values from ctrl_tuning.txt
+#theta = [0.69990, 1.33032, 1.87337, 2.34564, 2.78268, 3.18625, 3.55478, 6.41687, 8.55115, 10.35828, 11.97465, 13.46319, 14.85867, 16.18002, 17.44155, 18.65245, 19.81880, 20.94841, 22.04174, 23.10374]
+theta = [1.15891, 1.84772, 2.42077, 2.92690, 3.38802, 3.80657, 4.19594, 7.18473, 9.39732, 11.27620, 12.94861, 14.48495, 15.92136, 17.28167, 18.57785, 19.82184, 21.02006, 22.17886, 23.29936, 24.38449]
+#dQ_dtheta = [-1250.21385,-1294.80527,-1336.27132,-1391.04065,-1441.85613,-1486.51244,-1537.42141,-1817.22625,-2099.38349,-2355.84289,-2573.84846,-2803.55880,-3034.19836,-3248.08553,-3467.85105,-3684.03512,-3906.81836,-4126.44262,-4349.84614,-4582.75853]
+dQ_dtheta = [-1212.56150,-1260.38262,-1304.19749,-1345.73378,-1388.45006,-1432.11459,-1475.40984,-1776.32480,-2046.33655,-2298.30231,-2527.40144,-2751.31171,-2991.86210,-3205.72694,-3423.51267,-3638.49987,-3854.89341,-4074.27631,-4306.00544,-4537.38572]
 
 # Perform quadratic fit
 coefficients = np.polyfit(theta, dQ_dtheta, 2)  # 2 means quadratic
@@ -16,8 +17,8 @@ coefficients = np.polyfit(theta, dQ_dtheta, 2)  # 2 means quadratic
 KK2, KK1, intercept = coefficients  # From highest degree to lowest (x^2, x, constant)
 
 print('KK1:' + str(round(KK1,2)))
-print('KK2:' + str(round(KK2,2)))
-print('dQ_dtheta_0:' + str(round(intercept, 2)))
+print('KK2:' + str(round(KK2,3)))
+print('dQ_dtheta_0:' + str(round(intercept, 5)))
 
 # Generate a range of theta values for the fit line
 theta_fit = np.linspace(min(theta), max(theta), 500)
@@ -43,10 +44,9 @@ plt.text(0.05, 0.25, formula_text, fontsize=12, transform=plt.gca().transAxes, v
 
 # Show the plot
 plt.grid(True)
-
+plt.tight_layout()
 plt.savefig('A3 Controller design/Assignment/Figures/Part1_fit.svg', format='svg')
 plt.savefig('A3 Controller design/Assignment/Figures/Part1_fit.png', format='png')
-
 print(intercept/KK1)
 print(intercept/KK2)
 
@@ -59,17 +59,17 @@ rho = 1.225
 zeta = 0.7
 omega = np.array([0.05, 0.06])*(2*np.pi)
 I = txt_dict["Irotor_kg*m^2"]
-print(I)
+print(f'I: {I}')
 dQ_dOmega_0 = -1967.33604*1000
 P_r = 10000000
 omega_r = 8.627*(2*np.pi)/60 #from RPM to rad/s
 dQ_dOmega_op = -P_r/(omega_r**2)
-dQ_dtheta_0 = np.rad2deg(-1185.59951)*1000
+dQ_dtheta_0 = np.rad2deg(-1111.77678)*1000 #EDITED from -1185.59951 
 a_0 = KK2
 a_1 = KK1
 R = 92.4
-C_p_opt = 0.441
-lambda_opt = 7.5
+C_p_opt = 0.444             #EDITED from 0.441
+lambda_opt = 7.0            #EDITED from 7.5
 
 K_opt = (eta*rho*np.pi*R**5*C_p_opt)/(2*lambda_opt**3)
 print('{:.3e}'.format(K_opt))
